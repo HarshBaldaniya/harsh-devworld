13479e3cd2d84ad02dbfef72cfa10be71ce4b9fe
HarshBaldaniya <baldaniyaharsh5@gmail.com>
2025-07-21 23:41:43 +0530

    fix: added default export components for explore and resume pages

---
+++ src/pages/resume.tsx
@@ -0,0 +1,9 @@
+export default function ResumePage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Resume Page Coming Soon!
+      </h1>
+    </div>
+  );
+}
