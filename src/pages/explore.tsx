c40b75ca99437849f9e23cb1fb23bffe628cb0d7
HarshBaldaniya <baldaniyaharsh5@gmail.com>
2025-07-22 21:41:05 +0530

    Reinitialized project with fresh Next.js setup

--- src/pages/explore.tsx
+++
@@ -1,9 +0,0 @@
-export default function ExplorePage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-green-500">
-        Explore DevWorld Coming Soon!
-      </h1>
-    </div>
-  );
-}
